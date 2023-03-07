90bb0cecfafdd02c156a0c3e5e7d29cc923be1cc
Marcos Chamosa Rodríguez <a21marcoscr1@a21eql08.sanclemente.local>
2023-03-07 12:09:18 +0100

    otro commit

--- time_off_manager/controllers/controllers.py
+++ time_off_manager/controllers/controllers.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
 
 
-# class Mimodulo(http.Controller):
-#     @http.route('/mimodulo/mimodulo/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class time_off_manager(http.Controller):
+    @http.route('/time_off_manager/time_off_manager/', auth='public')
+    def index(self, **kw):
+        return "Hello, world"
 
-#     @http.route('/mimodulo/mimodulo/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('mimodulo.listing', {
-#             'root': '/mimodulo/mimodulo',
-#             'objects': http.request.env['mimodulo.mimodulo'].search([]),
-#         })
-
-#     @http.route('/mimodulo/mimodulo/objects/<model("mimodulo.mimodulo"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('mimodulo.object', {
-#             'object': obj
-#         })
+    @http.route('/time_off_manager/time_off_manager/objects/', auth='public')
+    def list(self, **kw):
+        return http.request.render('time_off_manager.listing', {
+            'root': '/time_off_manager/time_off_manager',
+            'objects': http.request.env['time_off_manager.time_off_manager'].search([]),
+        })
+    @http.route('/time_off_manager/time_off_manager/objects/<model("time_off_manager.time_off_manager"):obj>/', auth='public')
+    def object(self, obj, **kw):
+        return http.request.render('time_off_manager.object', {
+            'object': obj
+        })
